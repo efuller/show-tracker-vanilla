56a3a340714408fb574405931e88131359ff1623
Eric Fuller <info@glassmoonmedia.com>
2018-06-12 08:18:19 -0400

    Better test the View constructor.

--- src/js/__tests__/controller.test.js
+++ src/js/__tests__/controller.test.js
@@ -1,3 +1,4 @@
+import handlebars from 'handlebars';
 import Controller from '../Controller';
 import View from '../View';
 const mockRender = jest.fn();
@@ -11,6 +12,16 @@ jest.mock('../View', () => {
  * @todo Still do not full understand Mocks as well as properly testing this controller.
  */
 test('Controller.home() should render loading', () => {
+	const expectedHomeTmpl = `<div class="page">
+	{{> header}}
+	{{> searchForm}}
+	{{> results}}
+	{{> showDetails}}
+</div>
+`;
+
 	Controller.home();
+
+	expect(View.mock.calls[0][0]).toEqual(expectedHomeTmpl);
 	expect(mockRender.mock.calls[0][0]).toEqual('#app');
 });
