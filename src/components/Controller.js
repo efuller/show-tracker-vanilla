07c462275d3902e3489ff7aac37f8afe4b79896f
Eric Fuller <info@glassmoonmedia.com>
2018-06-19 20:37:52 -0400

    Remove unused functions and imports

--- src/components/Controller.js
+++ src/components/Controller.js
@@ -1,21 +1,7 @@
 import Main from '../views/main.hbs';
-import Results from '../views/partials/results.hbs';
-import { getShows } from '../API';
 import HomeView from './HomeView';
-import SearchResults from './SearchResultsView';
 
 export const home = () => {
-	const view = new HomeView(Main, {});
-	view.render('#app');
+	const view = new HomeView('#app', Main, {});
 	view.bindEvents();
-};
-
-export const getSearchResults = (show) => {
-	getShows(show)
-		.then((res) => {
-			const shows = res.data.results || [];
-			console.log(shows);
-			const view = new SearchResults(Results, shows);
-			view.render('#search-results');
-		});
 };
